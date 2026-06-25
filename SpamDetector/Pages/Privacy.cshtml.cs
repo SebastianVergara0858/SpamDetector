ed0f8ead0219b682a0baa45f60f67f6b2dce1917
Sebastian Vergara <sebastian.vergara@my.unitec.edu.mx>
2026-06-24 20:10:38 -0600

    Proyecto ASP.NET Core MVC creado

---
+++ SpamDetector/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpamDetector.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
