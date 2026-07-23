ae5be277f19353677963d2d10ebf9e4ff3f7cbcd
Sebastian Vergara <sebastian.vergara@my.unitec.edu.mx>
2026-07-22 20:33:13 -0600

    feat: agregar respuesta personalizada para peticiones bloqueadas (429)
    - Configuración del evento OnRejected en el RateLimiter para devolver un mensaje claro y estructurado cuando un usuario excede el límite de peticiones.

--- SpamDetector/Program.cs
+++ SpamDetector/Program.cs
@@ -2,21 +2,44 @@ using Microsoft.EntityFrameworkCore;
 using SpamDetector.Data;
 using SpamDetector.Middleware;
 using SpamDetector.Services;
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// Registrar el detector de spam en el contenedor de dependencias
+
 builder.Services.AddScoped<ISpamDetectorService, SpamDetectorService>();
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+    options.OnRejected = async (context, cancellationToken) =>
+    {
+        context.HttpContext.Response.ContentType = "application/json";
+
+        var response = new
+        {
+            success = false,
+            message = "Has realizado demasiadas peticiones. Por favor, inténtalo de nuevo en unos momentos.",
+            errorCode = 429
+        };
+
+        await context.HttpContext.Response.WriteAsJsonAsync(response, cancellationToken);
+    };
+
+});
+
+
 var app = builder.Build();
-app.UseMiddleware<SpamBlockerMiddleware>();
-// Configure the HTTP request pipeline.
+
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -25,9 +48,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseRateLimiter();
+
+app.UseMiddleware<SpamBlockerMiddleware>();
+
 app.UseAuthorization();
 
 app.MapRazorPages();
 
 app.Run();
-
